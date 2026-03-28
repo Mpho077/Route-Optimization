29e70ad2ea68506e5f1e61d9456c937c6cb16b2c
Mpho077 <69971504+Mpho077@users.noreply.github.com>
2026-03-28 10:35:24 +0200

    Delete route_optimization directory

--- route_optimization/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import project_task
-from . import route_plan
-from . import res_config_settings
-from . import route_optimizer
--- route_optimization/providers/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import base_provider
-from . import osrm_provider
-from . import google_maps_provider
-from . import provider_factory
